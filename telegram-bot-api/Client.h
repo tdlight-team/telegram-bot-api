c3999a2144bd3efd4a3b6b7c30c8c008597ddf84
levlam <levlam@telegram.org>
2023-11-13 22:08:07 +0300

    Add message.link_preview_options.

--- telegram-bot-api/Client.h
+++ telegram-bot-api/Client.h
@@ -110,6 +110,7 @@ class Client final : public WebhookActor::Callback {
   class JsonChatInviteLink;
   class JsonChat;
   class JsonMessageSender;
+  class JsonLinkPreviewOptions;
   class JsonAnimation;
   class JsonAudio;
   class JsonDocument;
